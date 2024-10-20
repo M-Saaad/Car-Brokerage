3881a87bfaeff7bf2b30cdb4de17a074befec0a8
Muhanmmad Saad <saad992.sm@gmail.com>
2024-10-20 18:23:53 +0500

    typo in bash script

--- chatgpt_api.sh
+++ chatgpt_api.sh
@@ -1,2 +1,2 @@
-source /home/autobrokerai/Car-Brokerage/.venv/bin/python3
+source /home/autobrokerai/Car-Brokerage/.venv/bin/activate
 exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --reload
