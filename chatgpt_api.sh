c115ac117f29b0d97e84defd4076a92df0eb3ece
Muhanmmad Saad <saad992.sm@gmail.com>
2024-10-30 22:28:49 +0500

    add ssl certs

--- chatgpt_api.sh
+++ chatgpt_api.sh
@@ -1,2 +1,2 @@
 source /home/autobrokerai/Car-Brokerage/.venv/bin/activate
-exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --reload
+exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --ssl-keyfile /var/cpanel/ssl/apache_tls/autobrokerai.com/privkey.pem --ssl-certfile /var/cpanel/ssl/apache_tls/autobrokerai.com/fullchain.pem
