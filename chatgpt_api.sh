f949ef4249b2dc0d68f97f31805d1bbdf3f6f281
Muhanmmad Saad <saad992.sm@gmail.com>
2024-11-27 22:24:21 +0500

    update certs path in .sh file

--- chatgpt_api.sh
+++ chatgpt_api.sh
@@ -1,2 +1,2 @@
 source /home/autobrokerai/Car-Brokerage/.venv/bin/activate
-exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --ssl-keyfile /var/cpanel/ssl/apache_tls/autobrokerai.com/privkey.pem --ssl-certfile /var/cpanel/ssl/apache_tls/autobrokerai.com/fullchain.pem
+exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --ssl-keyfile certs/privkey.pem --ssl-certfile certs/fullchain.pem
