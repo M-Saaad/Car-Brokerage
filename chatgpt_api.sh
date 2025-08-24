f463468af40baa80055b99cb108d3523c9290898
Muhammad Saad <muhammad-saad@saad-P51.bbrouter>
2025-08-24 17:35:02 +0500

    change port to 8001

--- chatgpt_api.sh
+++ chatgpt_api.sh
@@ -1,2 +1,2 @@
 source /home/ec2-user/backend/Car-Brokerage/.venv/bin/activate
-exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --ssl-keyfile certs/privkey.pem --ssl-certfile certs/fullchain.pem
+exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8001 --ssl-keyfile certs/privkey.pem --ssl-certfile certs/fullchain.pem
