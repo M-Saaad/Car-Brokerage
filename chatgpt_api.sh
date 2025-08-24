72f49e0f6ee0d42c00d548c76bddada90e6e758d
Muhammad Saad <muhammad-saad@saad-P51.bbrouter>
2025-08-24 15:57:24 +0500

    Update path for AWS server in chatgpt_api.sh

--- chatgpt_api.sh
+++ chatgpt_api.sh
@@ -1,2 +1,2 @@
-source /home/autobrokerai/Car-Brokerage/.venv/bin/activate
+source /home/ec2-user/backend/Car-Brokerage/.venv/bin/activate
 exec uvicorn chatgpt_api:app --host 0.0.0.0 --port 8000 --ssl-keyfile certs/privkey.pem --ssl-certfile certs/fullchain.pem
